c4170797a212f0f309a0b6a0a12d66c7e93ca82e
Avnkarthik <annavarapukarthik0@gmail.com>
2025-06-18 06:48:17 +0530

    added session

--- server.ts
+++ server.ts
@@ -1,27 +1,35 @@
 import express from "express";
 import { controll } from "./routes";
-import { dbconnection } from "./database";
-import session from "express-session";
+import session, { SessionData } from "express-session";
 import MongoStore from "connect-mongo";
 import dotenv from "dotenv";
+import passport from "passport";
 dotenv.config();
 const app = express();
 
 
 app.use(express.json());
-const sessionOptions: session.SessionOptions = {
-  secret: process.env.SESSION_SECRET!, 
+app.use(session({
+  secret: process.env.SESSION_SECRET!,
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
   store: MongoStore.create({
     mongoUrl: process.env.dburl!,
+    dbName:"APPUSERS",
     collectionName: 'sessions'
   }),
   cookie: { maxAge: 1000 * 60 * 60 * 24 } 
-};
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+ passport.serializeUser((user,done)=> {
+     console.log({mssg:user});
+    done(null,user);
+   
+});
+  passport.deserializeUser((user:any,done)=>done(null,user));
+  
 
-app.use(session(sessionOptions));
-      
 
   app.use(controll)
     app.listen(8020, () => {
