1a461752fe377ca81ba9892aac2515e657243ce0
Avnkarthik <annavarapukarthik0@gmail.com>
2025-06-18 06:52:58 +0530

    Added routes for authentication

--- routes.ts
+++ routes.ts
@@ -2,8 +2,9 @@ import express,{Router} from "express";
 
 import { taskval, userSchema, userval } from "./middleware";
 import { validationResult } from "express-validator";
-import { gettask, inserttasks, updatetask, userlogin } from "./api";
+import { callbackTwitter, gettask, inserttasks, renotify, savesession, twitterauth, updatetask, userlogin } from "./api";
 import passport from "./OAuth";
+
 
  
 
@@ -13,9 +14,10 @@ controll.post("/insert",taskval,inserttasks );
 controll.put("/update:id",updatetask);
 controll.post("/userlogin",userlogin);
 
-controll.get("/google",passport.authenticate('google',{scope:['email','profile']}));
-controll.get("/auth/google",passport.authenticate('google',{successRedirect:"/dashboard/success",failureRedirect:"/dashboard/failure"}));
-controll.get("twitter",passport.authenticate('twitter'));
-controll.get("/auth/twitter",passport.authenticate('twitter',{successRedirect:"/dashboard/success",failureRedirect:"/dashboard/failure"}));
-controll.get("/facebook",passport.authenticate('facebook',{scope:['email','profile']}));
-controll.get("auth/facebook",passport.authenticate('facebook',{successRedirect:"/dashboard/success",failureRedirect:"/dashboard/failure"}));
+controll.get("/google",passport.authenticate('google',{scope:['email','profile','https://www.googleapis.com/auth/calendar'] ,prompt: 'consent',accessType: 'offline' }));
+controll.get("/auth/google",passport.authenticate('google',{failureRedirect:"/dashboard?failure"}),savesession);
+controll.get("/twitter",twitterauth);
+controll.get("/auth/twitter",callbackTwitter);
+controll.get("/facebook",passport.authenticate('facebook',{scope:['email','public_profile','user_events']}));
+controll.get("auth/facebook",passport.authenticate('facebook',{failureRedirect:"/dashboard?failure"}),savesession);
+controll.get("/refreshNotif",renotify)
