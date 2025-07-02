4f2fecfb6dff9d643a9601f62575b0a5d057f2eb
Avnkarthik <annavarapukarthik0@gmail.com>
2025-07-02 10:46:00 +0530

    Added functions for sessions and cookie parsing

--- server.ts
+++ server.ts
@@ -4,21 +4,33 @@ import session, { SessionData } from "express-session";
 import MongoStore from "connect-mongo";
 import dotenv from "dotenv";
 import passport from "passport";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+import { dbconnection, UnifiedEvent } from "./database";
 dotenv.config();
 const app = express();
 
 
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
+app.use(cookieParser())
 app.use(session({
   secret: process.env.SESSION_SECRET!,
-  resave: false,
-  saveUninitialized: false,
+  resave: true,
+  saveUninitialized:false,
   store: MongoStore.create({
     mongoUrl: process.env.dburl!,
     dbName:"APPUSERS",
     collectionName: 'sessions'
   }),
-  cookie: { maxAge: 1000 * 60 * 60 * 24 } 
+  cookie: { maxAge: 1000 * 60 * 60 * 24 ,
+    httpOnly: true,
+      secure: false,
+      sameSite:"lax"
+  } 
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -29,7 +41,7 @@ app.use(passport.session());
 });
   passport.deserializeUser((user:any,done)=>done(null,user));
   
-
+ 
 
   app.use(controll)
     app.listen(8020, () => {
@@ -40,3 +52,11 @@ app.use(passport.session());
 
 
 export default app;
+
+
+
+
+
+
+
+
