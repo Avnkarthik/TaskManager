3d5db69d6e8d1d6e58a9206b0881ffcc74823460
Avnkarthik <annavarapukarthik0@gmail.com>
2025-07-02 10:41:55 +0530

    Added schemas and models for unified events and users data with accss tokens

--- database.ts
+++ database.ts
@@ -57,21 +57,86 @@ let  events=new mongoose.Schema({
 });
 declare module 'express-session'{
      interface SessionData{
-        user?:{  
-                id?:String|undefined,
-               name?:String|undefined,
-                email?:String|undefined,
-                googleAccessToken?:String,
-                googleRefreshToken?:String,
-                facebookAccessToken?:String,
-                facebookRefreshToken?:String,
-                twitterAccessToken?:String,
-                twitterRefreshToken?:string,
-                codeVerifier?:String
-           };
+        user: {
+  id?: String;
+  name?: String;
+  email?: String;
+   codeVerifier?:String
+   
+
+  google?: {
+    googleAccessToken?: String;
+    googleRefreshToken?: String;
+  };
+  facebook?: {
+    facebookAccessToken?: String;
+    facebookRefreshToken?: String;
+  };
+  twitter?: {
+    twitterAccessToken?: string;
+    twitterRefreshToken?: string;
+  };
+ 
+}
+
     }
 
 
 }
+export const normDataSchema=new mongoose.Schema({
+
+  platform: { type: String, 
+            enum: ["Google", "Facebook", "Twitter"],
+            required: true
+         },
+  title: String,
+  description: String,
+  startTime: Date,
+  endTime: Date,
+  link: String,
+  sourceId: String,
+  raw: Object,
+  userId: { type: String, 
+          required: true
+ }
+});
+
+  
+
+export const UnifiedEvent= mongoose.model("UnifiedEvent", normDataSchema);
+
+
+
+
 export let Usermodel=model("user",user);
 export let TaskModel=model("Task",events);
+export interface FacebookUser{
+      id:String,
+   name:String,
+   email:String,
+   facebookAccessToken:String,
+   facebookRefreshToken:String
+  }
+  export interface GoogleUser{
+      id:String,
+   name:String,
+   email:String,
+   googleAccessToken:String,
+   googleRefreshToken:String
+  }
+  const Mergedschema=new mongoose.Schema({
+    id:String,
+    token:String,
+    name:String,
+    email:String,
+     googleAccessToken: String,
+    googleRefreshToken: String,
+     facebookAccessToken: String,
+    facebookRefreshToken: String,
+     twitterAccessToken: String,
+    twitterRefreshToken: String,
+    codeVerifier:String
+
+
+  });
+ export  const mergedModel=model("MergedSession",Mergedschema);
